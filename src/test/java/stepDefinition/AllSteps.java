b543048c3f04b982bb4b2b10729fe10a70024881
mhmmtkkl <makoklu32@gmail.com>
2020-04-25 15:41:57 -0400

    keywordDriven

---
+++ src/test/java/stepDefinition/AllSteps.java
@@ -0,0 +1,18 @@
+package stepDefinition;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+
+public class AllSteps {
+
+    @Given("^Navigete to URL$")
+    public void navigete_to_URL() {
+
+    }
+
+    @And("^Enter the \"([^\"]*)\"$")
+    public void enter_the(String arg1) {
+
+    }
+
+}
---
+++ target/test-classes/signIn.feature
@@ -0,0 +1,9 @@
+
+  Feature: Singing in the website
+
+    Scenario: Sign in to website
+
+      Given Navigete to URL
+      And Enter the "username"
+      And Enter the "password"
+
